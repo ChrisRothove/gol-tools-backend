ddd5580a66186cd0a5d46685b0a8c3dc8983226b
ChrisRothove <vacantorange@gmail.com>
2024-01-14 16:15:55 -0600

    remove indexRouter

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", corsMiddleware, indexRouter);
+// app.use("/", corsMiddleware, indexRouter);
 app.use("/users", corsMiddleware, usersRouter);
 
 // catch 404 and forward to error handler
