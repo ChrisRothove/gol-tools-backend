37c6b44c9a4e7c75f7dbb76e06c1207612411517
ChrisRothove <vacantorange@gmail.com>
2024-01-18 17:45:07 -0600

    log control origin and try specific origin

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://gol-tools.vercel.app/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
