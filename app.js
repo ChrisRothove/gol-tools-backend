adae3c1861f996f998364e337bceebf7e8792f5b
ChrisRothove <vacantorange@gmail.com>
2024-01-18 16:35:35 -0600

    added / route

--- app.js
+++ app.js
@@ -48,6 +48,10 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/users", usersRouter);
 
+app.use("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
