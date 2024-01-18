873c954599f928189038539c7c7a9d674685640c
ChrisRothove <vacantorange@gmail.com>
2024-01-18 16:53:52 -0600

    added cors origins back in

--- app.js
+++ app.js
@@ -13,28 +13,28 @@ var app = express();
 // setup CORS
 var cors = require("cors");
 
-// var allowedOrigins = [
-//   "http://localhost:5173",
-//   "https://gol-tools-backend.vercel.app/",
-//   "https://gol-tools.vercel.app/",
-// ];
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       // allow requests with no origin
-//       // (like mobile apps or curl requests)
-//       // if(!origin) return callback(null, true);
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         var msg =
-//           "The CORS policy for this site does not " +
-//           "allow access from the specified Origin.";
-//         return callback(new Error(msg), false);
-//       }
-//       return callback(null, true);
-//     },
-//   })
-// );
+var allowedOrigins = [
+  "http://localhost:5173",
+  "https://gol-tools-backend.vercel.app/",
+  "https://gol-tools.vercel.app/",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      // if(!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 require("dotenv").config();
 
